5a17751305f990cec097e5c55ba045f9f91e3d09
shuvo794 <shuvohossain794@gmail.com>
2024-04-09 17:36:20 +0600

    send

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import "./App.css";
-
+import Button from "@mui/material/Button";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl text-red-500 font-bold underline">
+        Hello world!
+      </h1>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </>
   );
 }
