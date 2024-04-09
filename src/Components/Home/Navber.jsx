2d570a279d5838915e2fa00998facd37614f9068
shuvo794 <shuvohossain794@gmail.com>
2024-04-09 18:12:31 +0600

    send

--- src/Components/Home/Navber.jsx
+++ src/Components/Home/Navber.jsx
@@ -2,11 +2,12 @@ import { styled, alpha } from "@mui/material/styles";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
+
 import Typography from "@mui/material/Typography";
 import InputBase from "@mui/material/InputBase";
-import MenuIcon from "@mui/icons-material/Menu";
+
 import SearchIcon from "@mui/icons-material/Search";
+import { Link } from "react-router-dom";
 
 const Search = styled("div")(({ theme }) => ({
   position: "relative",
@@ -54,15 +55,6 @@ const Navber = () => {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography
             variant="h6"
             noWrap
@@ -71,6 +63,36 @@ const Navber = () => {
           >
             MUI
           </Typography>
+          <Link
+            style={{
+              textDecoration: "none",
+              color: "white",
+              fontSize: "20px",
+              marginRight: "20px",
+            }}
+          >
+            Home
+          </Link>
+          <Link
+            style={{
+              textDecoration: "none",
+              color: "white",
+              fontSize: "20px",
+              marginRight: "20px",
+            }}
+          >
+            Home
+          </Link>
+          <Link
+            style={{
+              textDecoration: "none",
+              color: "white",
+              fontSize: "20px",
+              marginRight: "20px",
+            }}
+          >
+            Home
+          </Link>
           <Search>
             <SearchIconWrapper>
               <SearchIcon />
