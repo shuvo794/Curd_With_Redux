4edf9e4051ba8c60b1b99de642155f8b182deba4
shuvo794 <shuvohossain794@gmail.com>
2024-04-09 17:59:01 +0600

    send

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import App from "./App.jsx";
 import "./index.css";
 import { store } from "./app/store/store.js";
 import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Routes/Routes.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
