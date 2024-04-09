8cde634bc04b7fc1083c27db036f4194fb6d8da7
shuvo794 <shuvohossain794@gmail.com>
2024-04-09 12:01:16 +0600

    create store fi;e

---
+++ src/app/features/userDetailSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userDetailSlice = createSlice({
+  name: "userDetail",
+  initialState: {
+    users: [],
+    loading: false,
+    error: null,
+  },
+});
