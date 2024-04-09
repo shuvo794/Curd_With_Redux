c733c706aafba640c57e656be1b05e34c77c0954
shuvo794 <shuvohossain794@gmail.com>
2024-04-09 18:04:24 +0600

    send

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,7 +1,9 @@
 import { Outlet } from "react-router-dom";
+import Navber from "../Components/Home/Navber";
 const Main = () => {
   return (
     <div>
+      <Navber></Navber>
       <Outlet></Outlet>
     </div>
   );
