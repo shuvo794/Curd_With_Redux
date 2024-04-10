e687e0606aff8042940f67d03f32b64f9065ba89
shuvo794 <shuvohossain794@gmail.com>
2024-04-10 12:08:24 +0600

    send

--- src/app/features/userDetailSlice.js
+++ src/app/features/userDetailSlice.js
@@ -1,4 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const createUser = createAsyncThunk(
+  "createuser",
+  async (data, { rejectWithValue }) => {
+    const res = await fetch(
+      "https://6614ff462fc47b4cf27dae4a.mockapi.io/curd",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: "test",
+        }),
+      }
+    );
+
+    try {
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
 
 export const userDetailSlice = createSlice({
   name: "userDetail",
