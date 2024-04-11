e33b1937cf8d5760871ef6601b366f5620182772
shuvo794 <shuvohossain794@gmail.com>
2024-04-11 10:22:23 +0600

    send

--- src/Components/Update/Update.jsx
+++ src/Components/Update/Update.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-duplicate-props */
 import { Grid, Paper, Typography, TextField, Button } from "@material-ui/core";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
@@ -44,6 +45,7 @@ const Update = () => {
             label="Name"
             name="name"
             placeholder="Enter your name"
+            value={updateData && updateData[0].name}
             // onChange={getUserData}
           />
           <TextField
@@ -51,13 +53,15 @@ const Update = () => {
             label="Email"
             name="email"
             placeholder="Enter your email"
-            // onChange={getUserData}
+            value={updateData && updateData[0].email}
+            // onChange={getUserData}/
           />
           <TextField
             fullWidth
             label="age"
             name="age"
             placeholder="Enter your Age"
+            value={updateData && updateData[0].age}
             // onChange={getUserData}
           />
           <FormControl component="fieldset" style={marginTop}>
@@ -71,6 +75,7 @@ const Update = () => {
                 name="gender"
                 value="female"
                 control={<Radio />}
+                checked={updateData && updateData[0].gender === "female"}
                 label="Female"
                 // onChange={getUserData}
               />
@@ -78,6 +83,7 @@ const Update = () => {
                 name="gender"
                 value="male"
                 control={<Radio />}
+                checked={updateData && updateData[0].gender === "male"}
                 label="Male"
                 // onChange={getUserData}
               />
